0a8659292a31ca3cd671f2f72e8ed68d8cc735ef
수빈 <inbv4311@naver.com>
2024-03-05 23:21:35 +0900

    게시판모델  _ git test

---
+++ app/Models/BoardModel.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class BoardModel extends Model
+{
+    // protected $tableName;
+    protected $table;
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['title','content','author','update_author','created_at','updated_at','used','hit','likes','bigo1','bigo2','bigo3'];
+
+    protected bool $allowEmptyInserts = false;
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    public function setTableName($table)
+    {
+        $this->table = $table;
+    }
+
+    public function getTableName()
+    {
+        return $this->table;
+    }
+}
