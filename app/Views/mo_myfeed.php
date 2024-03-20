8b5cce61fe5739c5e5fbe3cbf59ff93410843647
서승표 <nonamedm@naver.com>
2024-03-20 15:54:00 +0900

    피드 리스트 영상오류수정

--- app/Views/mo_myfeed.php
+++ app/Views/mo_myfeed.php
@@ -65,21 +65,18 @@
                                     <?php foreach ($feed_list as $feed): ?>
                                         <a onclick="showFeedDetail('feedDetail', '<?= $feed['feed_idx'] ?>') ">
                                             <?php if($feed['public_yn']==='0') { ?>
-                                            <?php 
-                                                function isMobileDevice() {
-                                                    return preg_match('/(android|iphone|ipod|ipad|windows phone|iemobile|opera mini)/i', $_SERVER['HTTP_USER_AGENT']);
-                                                }
-                                                $patternImg = "/\.(jpg|jpeg|png|gif|bmp|tiff|tif|webp|svg)$/i";
-                                                $patternMov = "/\.(mp4|avi|mov|mkv|flv|wmv|webm)$/i";
-                                                $patternOnlyMov = "/\.(mov)$/i";
-                                            ?>
-                                            <?php if(isMobileDevice()&&preg_match($patternOnlyMov,$feed['thumb_filename'])) { ?>
-                                                <img src="/<?= $feed['thumb_filepath']?><?= $feed['thumb_filename']?>"/>
-                                            <?php } else if (preg_match($patternMov,$feed['thumb_filename'])) { ?>
-                                                <video src="/<?= $feed['thumb_filepath']?><?= $feed['thumb_filename']?>" autoplay></video>
-                                            <?php } else if (preg_match($patternImg,$feed['thumb_filename'])) { ?>
-                                                <img src="/<?= $feed['thumb_filepath']?><?= $feed['thumb_filename']?>" />
-                                            <?php } ?>
+                                                <?php 
+                                                    $patternImg = "/\.(jpg|jpeg|png|gif|bmp|tiff|tif|webp|svg)$/i";
+                                                    $patternMov = "/\.(mp4|avi|mov|mkv|flv|wmv|webm)$/i";
+                                                    $patternOnlyMov = "/\.(mov)$/i";
+                                                ?>
+                                                <?php if(isMobileDevice()&&preg_match($patternOnlyMov,$feed['thumb_filename'])) { ?>
+                                                    <img src="/<?= $feed['thumb_filepath']?><?= $feed['thumb_filename']?>"/>
+                                                <?php } else if (preg_match($patternMov,$feed['thumb_filename'])) { ?>
+                                                    <video src="/<?= $feed['thumb_filepath']?><?= $feed['thumb_filename']?>"></video>
+                                                <?php } else if (preg_match($patternImg,$feed['thumb_filename'])) { ?>
+                                                    <img src="/<?= $feed['thumb_filepath']?><?= $feed['thumb_filename']?>" />
+                                                <?php } ?>
 
                                             <?php } else { ?>
                                                 <img src="/static/images/profile_img_private.png" />
