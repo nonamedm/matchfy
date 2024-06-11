b41838bb2a6255ea25e9d8b04be44c00cecf9721
서승표 <nonamedm@naver.com>
2024-06-11 14:21:05 +0900

    구글 애널리틱스 테스트스크립트 적용

--- app/Views/index.php
+++ app/Views/index.php
@@ -60,6 +60,19 @@
             }
         }
     </style>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16592783156">
+    </script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'AW-16592783156');
+    </script>
 </head>
 <?php
 $session = session();
