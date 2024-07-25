b963776499ae5ad07c065a2ff3b8d6fcfd70346e
서승표 <nonamedm@naver.com>
2024-07-25 10:52:34 +0900

    관리자페이지 사이드메뉴 리스트 수정

--- app/Views/admin/header.php
+++ app/Views/admin/header.php
@@ -3,11 +3,24 @@
         <img src="/static/images/matchfy.png" style="width:58px;">
         <p style="padding: 0;font-size: larger;font-weight: 900;margin: 0;">관리자페이지</p>
     </div>
+
+    <h4 class="menu_binder">매치파이 관리</h4>
     <li class="menu_item">
-        <a href="/ad/faq/faqList"><?= lang('Admin.faq') ?></a>
+        <a href="/ad/member/memberMngment">회원 관리</a>
     </li>
     <li class="menu_item">
+        <a href="/ad/member/memberMngment">모임 관리</a>
+    </li>
+    <li class="menu_item">
+        <a href="/ad/member/memberMngment">푸시/팝업 관리</a>
+    </li>
+
+    <h4 class="menu_binder">고객 지원</h4>
+    <li class="menu_item">
         <a href="/ad/notice/noticeList"><?= lang('Admin.notice') ?></a>
+    </li>
+    <li class="menu_item">
+        <a href="/ad/faq/faqList"><?= lang('Admin.faq') ?></a>
     </li>
     <li class="menu_item">
         <a href="/ad/terms/termsList"><?= lang('Admin.terms') ?></a>
@@ -15,39 +28,44 @@
     <li class="menu_item">
         <a href="/ad/privacy/privacyList"><?= lang('Admin.privacy') ?></a>
     </li>
+
+    <h4 class="menu_binder">시스템 관리</h4>
     <li class="menu_item">
+        <a href="/ad/privacy/privacyList">계정 관리</a>
+    </li>
+    <li class="menu_item">
+        <a href="/ad/privacy/privacyList">버전 관리</a>
+    </li>
+    <!-- <li class="menu_item">
         <a href="/ad/exchange/exchangeList"><?= lang('Admin.exchange') ?></a>
     </li>
     <li class="menu_item">
         <a href="/ad/alliance/allianceList"><?= lang('Admin.alliancePay') ?></a>
-    </li>
-    <li class="menu_item">
+    </li> -->
+    <!-- <li class="menu_item">
         <a href="/ad/member/memberApproveList">회원 승인목록</a>
-    </li>
-    <li class="menu_item">
-        <a href="/ad/member/memberMngment">회원 관리</a>
-    </li>
+    </li> -->
     <!-- <li class="menu_item">
         <a href="/ad/member/memberPaymentList">회원 계좌이체 승인 목록</a>
     </li> -->
-    <li class="menu_item">
+    <!-- <li class="menu_item">
         <a href="/ad/report/reportList">회원 신고 목록</a>
-    </li>
-    <li class="menu_item">
+    </li> -->
+    <!-- <li class="menu_item">
         <a href="/ad/intro/newsList">회사 소개 MEDIA 목록</a>
-    </li>
-    <li class="menu_item">
+    </li> -->
+    <!-- <li class="menu_item">
         <a href="/ad/support/noticeList">서포터즈 공지목록</a>
-    </li>
-    <li class="menu_item">
+    </li> -->
+    <!-- <li class="menu_item">
         <a href="/ad/support/faqList">서포터즈 FAQ목록</a>
-    </li>
-    <li class="menu_item">
+    </li> -->
+    <!-- <li class="menu_item">
         <a href="/ad/support/rewordList">서포터즈 리워드 관리</a>
-    </li>
-    <li class="menu_item">
+    </li> -->
+    <!-- <li class="menu_item">
         <a href="/ad/forkMngment">포크기능 on/off 관리</a>
-    </li>
+    </li> -->
     <!-- <li class="menu_item">
         <a href="/ad/partyMngment">파티관리자 기능</a>
     </li>
